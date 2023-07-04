ae53cf25a612f655fbdeae061d030f35ec3d59e0
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2023-07-04 19:18:22 +0200

    added server api urls

--- lib/services/api_path.dart
+++ lib/services/api_path.dart
@@ -1,4 +1,4 @@
-class APIPath {
+class SpotifyAPIPaths {
   static final List<String> _scopes = [
     'user-read-private',
     'user-read-email',
@@ -19,4 +19,14 @@ class APIPath {
   static String play = 'https://api.spotify.com/v1/me/player/play';
   static String pause = 'https://api.spotify.com/v1/me/player/pause';
   static String player = 'https://api.spotify.com/v1/me/player';
+}
+
+class ServerAPIPaths {
+  static String serverUrl = 'http://127.0.0.1:8000/';
+
+  static String requestTokens = '$serverUrl/api/token';
+
+  static String login = '$serverUrl/api/db/login/';
+  static String register = '$serverUrl/api/db/register/';
+  static String checkEmail = '$serverUrl/api/db/emailExists/';
 }
