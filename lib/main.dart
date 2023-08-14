3a0aaf8c2d3676bdab358b0a5fe043d667ce8ab7
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2023-08-14 13:04:34 +0200

    dotenv working, bug with data from spotify

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'app.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
+  debugPrint(dotenv.env['SERVER_URL'].toString());
   runApp(const MyApp());
 }
 
