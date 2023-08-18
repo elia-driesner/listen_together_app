159ba55e38f64cd84340cf559da59a848d7317a9
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2023-08-18 16:48:07 +0200

    added websocket package

---
+++ packages/websockets/lib/websockets.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:web_socket_channel/web_socket_channel.dart';
+// import 'package:listen_together_app/services/secure_storage.dart';
+
+class Websocket {
+  static String serverUrl = dotenv.env['WEBSOCKETS_URL'].toString();
+  static String playingSongUrl = dotenv.env['PLAYING_SONG_URL'].toString();
+  static var channel;
+
+  static void init() {
+    channel = WebSocketChannel.connect(
+      Uri.parse(serverUrl + playingSongUrl),
+    );
+  }
+
+  static void send(data) => channel.sink.add(data);
+
+  static void disconnect() => channel.sink.close();
+}
